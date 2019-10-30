7a97c1e97aba57bc9b567306215e2a7af5131edf
Michael Li <limichael1099419@gmail.com>
2019-10-30 12:27:10 -0700

    with background

--- supermariobros.py
+++ supermariobros.py
@@ -17,6 +17,9 @@ def run_game():
     stats = Stats()
 
     mario = SuperMario(screen)
+    mario.rect.x = screen.get_rect().centerx
+    mario.rect.y = GROUND_HEIGHT * BG_SCALER
+
     clock = pygame.time.Clock()
     bg = Level1(screen=screen)
 
@@ -38,7 +41,7 @@ def run_game():
         mario.update_walking(stats)
         frame_index += 1
         bg.draw(mario=mario)
-        bg.blit_rect(frame_index=frame_index)
+        bg.blit_rect(frame_index=frame_index, mario=mario)
         gf.update_screen(stats, mario)
 
         pygame.display.flip()
